a56f9807e15f547cf205498b89d19ee6a16fc5f9
Esther Kang <estherkang14@gmail.com>
2020-05-18 21:05:00 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -4,21 +4,21 @@ def first_challenge
        patriarch: {
          name: "Lord Montague", 
          age: "53"
-       }
+       },
        matriarch: {
          name: "Lady Montague",
          age: "54"
-       }
+       },
        hero: {
          name: "Romeo",
          age: "15",
          status: "alive"
-       }
+       },
        heros_friends: [
          {
            name: "Benvolio",
            age: "17",
-           attitude: worried
+           attitude: "worried"
          },
          {
            name: "Mercutio",
@@ -27,6 +27,21 @@ def first_challenge
          }
        ]
      },
-     capulet: 
+     capulet: {
+       patriarch: {
+         name: "Lord Capulet",
+         age: "50"
+       },
+       matriarch: {
+         name: "Lady Capulet",
+         age: "51"
+       },
+       heroine: {
+         name: "Juliet",
+         age: "15",
+         status: "Alive"
+       },
+       
+     }
    }
 end
